43f4731a8fe7c00787ffeaac5900ade609ae6722
Matvei <matvesch@protonmail.com>
2021-12-02 11:04:22 +0100

    Add files via upload

--- C02/C02/ex11/ft_putstr_non_printable.c
+++ C02/C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: mvoinich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 10:11:51 by mvoinich          #+#    #+#             */
-/*   Updated: 2021/12/01 13:17:49 by mvoinich         ###   ########.fr       */
+/*   Updated: 2021/12/02 11:04:03 by mvoinich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 32 && str[i] <= 127)
-		{
-			ft_putchar(str[i]);
-		}
-		else
+		if (str[i] < 32 || str[i] > 126)
 		{
 			ft_putchar('\\');
 			ft_putchar("0123456789abcdef"[str[i] / 16]);
 			ft_putchar("0123456789abcdef"[str[i] % 16]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
 		}
 		i++;
 	}
