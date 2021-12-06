17352c9941c9a86c3820ae77e8734e42c08bdc7a
Matvei <matvesch@protonmail.com>
2021-12-06 11:49:57 +0100

    right version

--- C03/C03_dev/ex01/ft_strncmp.c
+++ C03/C03_dev/ex01/ft_strncmp.c
@@ -6,43 +6,41 @@
 /*   By: mvoinich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 14:04:11 by mvoinich          #+#    #+#             */
-/*   Updated: 2021/12/01 14:11:24 by mvoinich         ###   ########.fr       */
+/*   Updated: 2021/12/06 11:49:29 by mvoinich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
-	int	flag;
+	int				flag;
 
 	i = 0;
 	flag = 0;
-	while (i < n)
+	while (((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i]) && n > 0)
 	{
-		if (s1[i] == s2[i])
-		{
-			flag = 0;
-		}
-		else if (s1[i] > s2[i])
-		{
-			flag = 1;
-			break ;
-		}
-		else if (s1[i] < s2[i])
-		{
-			flag = -1;
-			break ;
-		}
 		i++;
+		n--;
+	}
+	if (s1[i] == s2[i] || n == 0)
+	{
+		flag = 0;
+	}
+	else
+	{
+		flag = s1[i] - s2[i];
 	}
 	return (flag);
 }
+		
 
 int	main()
 {
-	char s1[] = "helLO";
+	char s1[] = "helLo";
 	char s2[] = "hello";
+	printf("%i\n", strncmp(s1, s2, 5));
 	printf("%i\n", ft_strncmp(s1, s2, 5));
 }
