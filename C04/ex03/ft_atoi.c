1a6d25ab39e803a5cd83304dff5169062ddae400
Matvei <matvesch@protonmail.com>
2021-12-09 15:25:56 +0100

    ready to eval

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvoinich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/03 11:13:30 by mvoinich          #+#    #+#             */
-/*   Updated: 2021/12/04 10:29:14 by mvoinich         ###   ########.fr       */
+/*   Created: 2021/12/09 15:21:00 by mvoinich          #+#    #+#             */
+/*   Updated: 2021/12/09 15:25:39 by mvoinich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,33 @@
 int	ft_atoi(char *str)
 {
 	int	i;
-	int min_am;
 	int	sign;
-	int number;
+	int	number;
 
 	i = 0;
-	min_am = 0;
 	sign = 1;
 	number = 0;
-	while (str[i] != '\0')
+	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
+		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
+		i++;
+	while ((str[i] == '-') || (str[i] == '+'))
 	{
 		if (str[i] == '-')
-		{
-			min_am++;
-		}
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			number = number * 10 + str[i] - '0';
-			if (!(str[i + 1] >= '0' && str[i + 1] <= '9'))
-			{
-				break ;
-			}
-		}
+			sign *= -1;
 		i++;
 	}
-	if (min_am % 2 != 0)
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		sign = -1;
+		number = (number * 10) + (str[i] - '0');
+		i++;
 	}
-	return (number * sign);
+	number = number * sign;
+	return (number);
 }
 
 int	main()
 {
-	char str[] = " ---+--+1234ab567";
+	char str[] =  " ---+--+1234ab567";
 	int num = ft_atoi(str);
 	printf("%i\n", num);
 	return (0);
