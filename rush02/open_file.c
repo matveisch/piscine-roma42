8beee9e25da8fe3f8e4d78f67e8fb777202a6a5b
Matvei <matvesch@protonmail.com>
2021-12-11 13:52:53 +0100

    Add files via upload

--- rush02/open_file.c
+++ rush02/open_file.c
@@ -4,14 +4,12 @@
 
 int main()
 {
-	int fh;
-	int rd;
-	char buffer[2048];
+	int		fh;
+	int		rd;
+	char	buffer[2048];
 	
 	fh = open("numbers.dict", O_RDONLY);
-	while ((rd = read(fh, buffer, 2048)) != 0)
-	{
-		buffer[rd] = '\0';
-		printf("%s", buffer);
-	}
+	rd = read(fh, buffer, 2048);
+	buffer[rd] = '\0';
+	printf("%s", buffer);
 }
